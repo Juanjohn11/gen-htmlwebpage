54df69619a66aa7424bfec7314865d8132ca61b6
Juanjohn11 <elmatanaco1144@gmail.com>
2022-01-06 13:58:40 -0800

    adjusted code for the generated html temp

--- src/genHtml.js
+++ src/genHtml.js
@@ -20,8 +20,8 @@ const generateTeamPage = function (employeeCards) {
                 <div class="row justify-content-center" id="team-cards">
                     <!--Team Cards-->
                     ${employeeCards.filter(emp => emp.getRole()=== "Manager").map(generateManager).join("\n")}
-                    ${""/*employeeCards.filter(emp => emp.getRole()=== "Intern").map(generateManager).join("\n")*/}
-                    ${""/*employeeCards.filter(emp => emp.getRole()=== "Engineer").map(generateManager).join("\n")*/}
+                    ${employeeCards.filter(emp => emp.getRole()=== "Intern").map(generateIntern).join("\n")}
+                    ${employeeCards.filter(emp => emp.getRole()=== "Engineer").map(generateEngineer).join("\n")}
 
 
                 </div>
@@ -40,12 +40,46 @@ const generateTeamPage = function (employeeCards) {
         <div class="card h-100">
             <div class="card-header">
                 <h3>${manager.name}</h3>
-                <h4>Manager</h4><i class="material-icons">content_paste</i>
+                <h4>Manager</h4>
             </div>
             <div class="card-body">
                 <p class="id">ID: ${manager.id}</p>
                 <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
                 <p class="office">Office Number: ${manager.officeNumber}</p>
+            </div>
+        </div>
+    </div>
+    `;
+}
+const generateIntern = function (intern) {
+    return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${intern.name}</h3>
+                <h4>Intern</h4>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${intern.id}</p>
+                <p class="email">Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
+                <p class="school">School: ${intern.school}</p>
+            </div>
+        </div>
+    </div>
+    `;
+}
+const generateEngineer = function (engineer) {
+    return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${engineer.name}</h3>
+                <h4>Engineer</h4>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${engineer.id}</p>
+                <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                <p class="github">github: ${engineer.github}</p>
             </div>
         </div>
     </div>
